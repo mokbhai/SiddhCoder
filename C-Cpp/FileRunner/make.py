bccbe7675a055d87d216e88bc24ab7b3cbf31c91
Mokshit Jain <66026094+mokbhai@users.noreply.github.com>
2023-08-02 13:20:41 +0000

    FileRunners are ready

--- C-Cpp/FileRunner/make.py
+++ C-Cpp/FileRunner/make.py
@@ -1,27 +1,47 @@
+"""
+C++ Compiler and Runner
+
+This script compiles and runs a C++ program provided as an argument and
+gives the option to delete the compiled executable afterward.
+"""
+
 import os
 import subprocess
+import sys
 
 def compile_and_run_cpp(filename):
+    """
+    Compile and run the C++ program.
+
+    Args:
+        filename (str): Name of the C++ source file.
+
+    Returns:
+        str or None: Returns the name of the compiled executable if successful, None otherwise.
+    """
     # Remove the ".cpp" extension from the filename
     executable_name = os.path.splitext(filename)[0]
 
-    # Compile the C++ file
     try:
-        subprocess.check_output(["g++", filename, "-o", executable_name])
-    except subprocess.CalledProcessError as e:
-        print(f"Compilation failed. Error: {e}")
-        return False
+        # Compile the C++ file
+        subprocess.run(["g++", filename, "-o", executable_name], check=True)
 
-    # Check if the compilation was successful
-    if not os.path.isfile(executable_name):
-        print("Compilation failed. Please check the C++ file for errors.")
-        return False
+        # Check if the compilation was successful
+        if not os.path.isfile(executable_name):
+            print("Compilation failed. Please check the C++ file for errors.")
+            return None
 
-    return executable_name
+        return executable_name
+
+    except subprocess.CalledProcessError as error:
+        print(f"Compilation failed. Error: {error}")
+        return None
 
 def main():
+    """
+    Main function to execute the script.
+    """
     # Check if a filename argument is provided
-    import sys
     if len(sys.argv) < 2:
         print("Please provide a filename as an argument.")
         sys.exit(1)
@@ -39,25 +59,26 @@ def main():
 
     # Execute the compiled program using the provided filename (without .cpp extension)
     try:
-        subprocess.run([executable_name])
-    except Exception as e:
-        print(f"Execution failed. Error: {e}")
-        return False
+        subprocess.run([executable_name], check=True)
+    except subprocess.CalledProcessError as error:
+        print(f"Execution failed. Error: {error}")
+        sys.exit(1)
 
     os.remove(executable_name)
 
     # Ask the user if they want to delete the compiled executable
-    # delete_choice = input(f"Do you want to delete the compiled executable '{executable_name}'? (y/n): ")
+    # delete_choice = input(f"Do you want to delete the compiled
+    #  executable '{executable_name}'? (y/n): ")
     # if delete_choice.lower() == "y":
     #     try:
     #         os.remove(executable_name)
     #         print(f"'{executable_name}' has been deleted.")
-    #     except Exception as e:
-    #         print(f"Failed to delete '{executable_name}'. Error: {e}")
+    #     except Exception as error:
+    #         print(f"Failed to delete '{executable_name}'. Error: {error}")
     # else:
     #     print(f"'{executable_name}' will be kept.")
 
-    # input("Press Enter to exit...")
+    input("Press Enter to exit...")
 
 if __name__ == "__main__":
     main()
