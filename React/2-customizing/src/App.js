d6d7f47aaa8abb70b238d06ec151bd6bf94aba4d
Developer <mokshitjain18@gmail.com>
2024-01-21 23:12:37 +0530

    week1 React by meta coursera

---
+++ React/2-customizing/src/App.js
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer";
+import Intro1 from "./components/Intro1";
+import Intro2 from "./components/Intro2";
+import Intro3 from "./components/Intro3";
+import Nav from "./components/Nav";
+import Promo from "./components/Promo";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
