efc42077207b111db2f16aecc2e787d639276d87
Developer <mokshitjain18@gmail.com>
2024-01-26 05:25:44 +0530

    week 1 completed react advance

---
+++ React/9-light-dark/src/ThemeContext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react"; //import
+
+const ThemeContext = createContext(undefined); // create context ko define kra
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme: () => setTheme(theme === "light" ? "dark" : "light"),
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
